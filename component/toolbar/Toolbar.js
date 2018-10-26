aa1a81677b09d13c0262661fa22ce4a7ea4818bf
BuildTools <unconfigured@null.spigotmc.org>
2018-10-26 13:39:43 +0200

    Work on Toolbar

--- component/toolbar/Toolbar.js
+++ component/toolbar/Toolbar.js
@@ -1,6 +1,7 @@
-Gui.Toolbar = function(upperText, upperProps, upperHoverProps, upperActiveProps, upperComponentHeight, minX, maxX){
+Gui.Toolbar = function(upperText, upperProps, upperHoverProps, upperActiveProps, upperComponentHeight, minX, maxX, upperMaxX, addComponents){
 	this.minX = minX;
 	this.maxX = maxX;
+	this.upperMaxX = upperMaxX;
 	this.upperY = 1 - upperComponentHeight;
 	this.upperComponent = new Gui.ActivatableTextComponent(upperText, props, hoverProps, activeProps, function(){
 		this.active = !this.active;
@@ -9,6 +10,7 @@ Gui.Toolbar = function(upperText, upperProps, upperHoverProps, upperActiveProps,
 		return this.active;
 	});
 	this.didInit = false;
+	this.addComponents = addComponents;
 	this.components = [];
 };
 
@@ -16,14 +18,21 @@ Gui.Toolbar.prototype.init = function(){
 	if(!this.didInit){
 		const y = this.upperY;
 		this.addComponent(this.upperComponent, this.upperY);
+
+		// The upper component is special
+		this.components[0].maxX = this.upperMaxX;
+		this.components[0].renderer.setBounds(this.minX, 1 - y, this.upperMaxX, 1);
+		this.components[0].state.setBounds(this.minX, 1 - y, this.upperMaxX, 1);
 		this.upperY = y;
+
+		// Now add the other components
 		this.addComponents();
 		this.didInit = true;
 	}
 };
 
 Gui.Toolbar.prototype.update = function(){
-	if(this.upperComponent.active){
+	if(this.upperComponent.isActive()){
 		const length = this.components.length;
 		for(let index = 0; index < length; index++){
 			if(this.components[index].component.update){
@@ -35,7 +44,7 @@ Gui.Toolbar.prototype.update = function(){
 
 Gui.Toolbar.prototype.render = function(renderer){
 	this.components[0].render(renderer);
-	if(this.upperComponent.active){
+	if(this.upperComponent.isActive()){
 		const length = this.components.length;
 		for(let index = 1; index < length; index++){
 			this.components[index].render(renderer);
@@ -45,7 +54,7 @@ Gui.Toolbar.prototype.render = function(renderer){
 
 Gui.Toolbar.prototype.click = function(x, y, button){
 	this.components[0].click(x, y, button);
-	if(this.upperComponent.active){
+	if(this.upperComponent.isActive()){
 		const length = this.components.length;
 		for(let index = 1; index < length; index++){
 			this.components[index].click(x, y, button);
@@ -67,7 +76,7 @@ Gui.Toolbar.prototype.clickOut = function(button){
 };
 
 Gui.Toolbar.prototype.keyType = function(key){
-	if(this.upperComponent.active){
+	if(this.upperComponent.isActive()){
 		const length = this.components.length;
 		for(let index = 0; index < length; index++){
 			if(this.components[index].component.keyType){
@@ -78,7 +87,7 @@ Gui.Toolbar.prototype.keyType = function(key){
 };
 
 Gui.Toolbar.prototype.keyDown = function(key){
-	if(this.upperComponent.active){
+	if(this.upperComponent.isActive()){
 		const length = this.components.length;
 		for(let index = 0; index < length; index++){
 			if(this.components[index].component.keyDown){
@@ -89,7 +98,7 @@ Gui.Toolbar.prototype.keyDown = function(key){
 };
 
 Gui.Toolbar.prototype.keyUp = function(key){
-	if(this.upperComponent.active){
+	if(this.upperComponent.isActive()){
 		const length = this.components.length;
 		for(let index = 0; index < length; index++){
 			if(this.components[index].component.keyUp){
