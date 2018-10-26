2c533420912a023dfdb734851e6652744d374e1f
BuildTools <unconfigured@null.spigotmc.org>
2018-10-26 13:39:14 +0200

    Add support for wrapper components without component

--- component/WrapperComponent.js
+++ component/WrapperComponent.js
@@ -3,42 +3,46 @@ Gui.WrapperComponent = function(component){
 };
 
 Gui.WrapperComponent.prototype.init = function(){
-	this.component.state = this.state;
-	if(this.component.init){
-		this.component.init();
+	if (this.component) {
+		this.component.state = this.state;
+		if (this.component.init) {
+			this.component.init();
+		}
 	}
 };
 
 Gui.WrapperComponent.prototype.render = function(renderer){
-	this.component.render(renderer);
+	if (this.component) {
+		this.component.render(renderer);
+	}
 };
 
 Gui.WrapperComponent.prototype.update = function(){
-	if(this.component.update){
+	if(this.component && this.component.update){
 		this.component.update();
 	}
 };
 
 Gui.WrapperComponent.prototype.click = function(x, y){
-	if(this.component.click){
+	if(this.component && this.component.click){
 		this.component.click(x, y);
 	}
 };
 
 Gui.WrapperComponent.prototype.keyUp = function(key){
-	if(this.component.keyUp){
+	if(this.component && this.component.keyUp){
 		this.component.keyUp(key);
 	}
 };
 
 Gui.WrapperComponent.prototype.keyDown = function(key){
-	if(this.component.keyDown){
+	if(this.component && this.component.keyDown){
 		this.component.keyDown(key);
 	}
 };
 
 Gui.WrapperComponent.prototype.keyType = function(key){
-	if(this.component.keyType){
+	if(this.component && this.component.keyType){
 		this.component.keyType(key);
 	}
 };
